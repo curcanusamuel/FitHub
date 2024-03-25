8556f7a970c94568b2ba54566d33c067797f768a
curcanusamuel <126792709+curcanusamuel@users.noreply.github.com>
2024-03-25 09:54:58 +0200

    Add files via upload

---
+++ spring/spring/src/main/java/com/example/spring/Service/ClientServiceImpl.java
@@ -0,0 +1,53 @@
+package com.example.spring.Service;
+
+import com.example.spring.Entity.Client;
+import com.example.spring.Repository.ClientRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * The ClientServiceImpl class implements the ClientService interface.
+ * It provides methods to manage Client entities by interacting with the ClientRepository.
+ */
+@Service
+@AllArgsConstructor
+public class ClientServiceImpl implements ClientService {
+
+    private ClientRepository clientRepository;
+
+    @Override
+    public Client createClient(Client client) {
+        return clientRepository.save(client);
+    }
+
+    @Override
+    public Client getClientById(int clientId) {
+        Optional<Client> optionalClient = clientRepository.findById(clientId);
+        return optionalClient.orElse(null);
+    }
+
+    @Override
+    public List<Client> getAllClients() {
+        return clientRepository.findAll();
+    }
+
+    @Override
+    public Client updateClient(Client client) {
+        Client existingClient = clientRepository.findById(client.getId()).orElse(null);
+        if (existingClient != null) {
+            existingClient.setName(client.getName());
+            existingClient.setAge(client.getAge());
+            existingClient.setTrainerId(client.getTrainerId());
+            return clientRepository.save(existingClient);
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteClient(int clientId) {
+        clientRepository.deleteById(clientId);
+    }
+}
