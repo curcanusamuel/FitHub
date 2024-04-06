6fea032a2da68c3c9d15c350c205466191cb0b11
curcanusamuel <126792709+curcanusamuel@users.noreply.github.com>
2024-04-06 17:08:33 +0300

    Delete spring directory

--- spring/JavaDoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.example.spring"},{"l":"com.example.spring.Controller"},{"l":"com.example.spring.Entity"},{"l":"com.example.spring.Repository"},{"l":"com.example.spring.Service"}];updateSearchResults();
