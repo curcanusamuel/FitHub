3717c7e8c4e7704c147b88b2043a3cf99f0bf496
curcanusamuel <126792709+curcanusamuel@users.noreply.github.com>
2024-04-07 16:04:48 +0300

    Add files via upload

---
+++ JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.example.spring"},{"l":"com.example.spring.Controller"},{"l":"com.example.spring.Entity"},{"l":"com.example.spring.Observer"},{"l":"com.example.spring.Repository"},{"l":"com.example.spring.Service"},{"l":"com.example.spring.Tests"}];updateSearchResults();
